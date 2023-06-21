8c910747a71325b7f03121add821b119c54449e0
LeeBG <donny1848@gmail.com>
2023-06-21 15:50:16 +0900

    크기가 작은 부분 문자열

---
+++ etc/src/smallSubstring/Main.java
@@ -0,0 +1,23 @@
+package smallSubstring;
+
+class Solution {
+    public int solution(String t, String p) {
+        int answer = 0;
+        int length = p.length();
+        
+        int num = 0;
+        System.out.println(num);
+        
+        return answer;
+    }
+}
+public class Main {
+	public static void main(String[] args) {
+		String t = "3141592";
+		String p = "271";
+		Solution s = new Solution();
+		int answer = s.solution(t, p);
+		System.out.println("answer : "+answer);
+		
+	}
+}
