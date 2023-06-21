2f54ba18782c5fa3da42672405d822efded5ae47
RightAccept <djfltrhkdeo46@gmail.com>
2023-06-22 00:40:03 +0900

    문자열 묶기

--- 프로그래머스/src/stringOverwrite/Main.java
+++ 프로그래머스/src/stringOverwrite/Main.java
@@ -2,6 +2,22 @@ package stringOverwrite;
 
 public class Main {
 	public static void main(String[] args) {
+		Solution s = new Solution();
+		String my_string;
+		String overwrite_string;
+		int n;
+		String result;
 		
+		my_string = "He11oWor1d";
+		overwrite_string = "lloWorl";
+		n = 2;
+		result = s.solution(my_string, overwrite_string, n);
+		System.out.println(result);
+		
+		my_string = "Program29b8UYP";
+		overwrite_string = "merS123";
+		n = 7;
+		result = s.solution(my_string, overwrite_string, n);
+		System.out.println(result);
 	}
 }
