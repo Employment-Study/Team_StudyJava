c2991e568914adbc74a6c88a64b1c7d92ef6fae7
LeeBG <donny1848@gmail.com>
2023-07-10 02:30:51 +0900

    숫자 짝꿍

--- etc/src/partnerNumber/Main.java
+++ etc/src/partnerNumber/Main.java
@@ -1,52 +1,46 @@
 package partnerNumber;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 // 숫자 짝꿍
 class Solution {
-    public String solution(String X, String Y) {
-        String answer = "-1";
-       
-        HashMap<Character, Integer> xMap = new HashMap();
-        for(int i=0;i<X.length();i++) {
-        	if(!xMap.containsKey(X.charAt(i))) {
-        		xMap.put(X.charAt(i), 1);
-        	}
-        	else {
-        		xMap.put(X.charAt(i), xMap.get(X.charAt(i))+1);
-        	}
-        }
-        
-        ArrayList<Integer> resultList = new ArrayList(); 
-        for(int i=0;i<Y.length();i++) {
-        	if(xMap.containsKey(Y.charAt(i))) {
-        		resultList.add(Y.charAt(i)-48);
-        		xMap.put(Y.charAt(i), xMap.get(Y.charAt(i))-1);
-        		if(xMap.get(Y.charAt(i)) == 0) {
-        			xMap.remove(Y.charAt(i));
-        		}
-        	}
-        }
-        
-        if(!resultList.isEmpty()) {
-        	long tmp = 0L;
-            resultList.sort(null);
-            for(int i=resultList.size()-1;i>=0;i--) {
-            	tmp += (long)(Math.pow(10, i)) * resultList.get(i);
-            }
-            answer = tmp+"";
-        }
-        
-        System.out.println(answer);
-        return answer;
-    }
+	public String solution(String X, String Y) {
+		String answer = "-1";
+		String[] sx = X.split("");
+		String[] sy = Y.split("");
+		StringBuilder sb = new StringBuilder();
+		
+		int[] arrResult = new int[10];
+		int[] xarr = new int[10];
+		int[] yarr = new int[10];
+		int result = 0;
+		
+		for(int i=0;i<sx.length;i++) {
+			xarr[Integer.parseInt(sx[i])]++;
+		}
+		
+		for(int i=0;i<sy.length;i++) {
+			yarr[Integer.parseInt(sy[i])]++;
+		}
+		
+		for(int i=9;i>=0;i--) {
+			while(xarr[i]>0 && yarr[i] > 0) {
+				sb.append(i);
+				
+				xarr[i]--;
+				yarr[i]--;
+			}
+		}
+		
+		answer = sb.toString();
+	
+		if(answer.equals("")) answer = "-1";
+		if(answer.charAt(0)=='0') answer = "0";
+		
+		return answer;
+	}
 }
 
 public class Main {
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		
-		s.solution("100", "203045");
+		s.solution("5525", "1255");
 	}
 }
